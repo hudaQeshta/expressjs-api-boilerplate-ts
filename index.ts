6b89984228b92a98bdf9a01cbb42107637b54c2f
msamgan <mohdsamgankhan@gmail.com>
2021-10-17 16:29:17 +0400

    removed unwanted code

--- index.ts
+++ index.ts
@@ -5,14 +5,8 @@ import express from 'express'
 import {api} from './routes/api'
 
 const app = express()
-// const port = process.env.PORT
 
 app.use(express.json())
-
-// console.log(`Node environment: ${process.env.NODE_ENV}`)
-// app.listen(port, () => {
-//     console.log(`Example app listening at port http://localhost:${port}`)
-// })
 
 api(app)
 
